aa065f3f48b349186bdb66e748dc03af1de32b5f
heyuno <9321103+heyuno@users.noreply.github.com>
2017-10-16 10:21:15 +0700

    Using spring boot application propterties

--- spring-gs-external-properties/src/main/java/com/learn/config/PropertyConfig.java
+++ spring-gs-external-properties/src/main/java/com/learn/config/PropertyConfig.java
@@ -1,25 +1,15 @@
 package com.learn.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 import com.learn.examplebean.FakeDataSource;
 import com.learn.examplebean.FakeUcBroker;
 
 @Configuration
-@PropertySources({
-	@PropertySource("classpath:datasource.properties"),
-	@PropertySource("classpath:ucbroker.properties")
-})
+
 public class PropertyConfig {
-	@Autowired
-	Environment env;
 
 	@Value("${learner.username}")
 	private String username;
@@ -42,7 +32,7 @@ public class PropertyConfig {
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource fakeDataSource = new FakeDataSource();
-		fakeDataSource.setUsername(env.getProperty("USERNAME"));
+		fakeDataSource.setUsername(username);
 		fakeDataSource.setPassword(password);
 		fakeDataSource.setUrl(dburl);
 		return fakeDataSource;
@@ -56,11 +46,4 @@ public class PropertyConfig {
 		fakeUcBroker.setUsername(ucUsername);
 		return fakeUcBroker;
 	}
-
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer properties() {
-		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-		return propertySourcesPlaceholderConfigurer;
-	}
-
 }
